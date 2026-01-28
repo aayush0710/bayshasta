3bdc86282a607d3e730d3286df950a63200b4c25
Aayush Gautam <aayush0710 + aayush0710@users.noreply.github.com>
2026-01-28 10:48:18 -0800

    Ignore Documents folder (fix pages build)

--- Documents/Visual Studio 2015/Projects/cis6spring2017AayushHw1Ex1.c/cis6spring2017AayushHw1Ex1.c/cis6spring2017AayushHw1Ex1.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main() {
-	printf("welcome to c world"!\n);
-	return 0;
-}
--- Documents/Visual Studio 2015/Projects/cis6spring2017AayushHw2Ex2.c/cis6spring2017AayushHw2Ex2.c/Source.cpp
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main() {
-	printf("cis6 Introduction to programming%\n");
-	returm 0;
-}
